de1d68ff7dd2d7acb2761a1575b451c3ff21ae3e
Abydenes <schoolreef2@protonmail.com>
2022-07-28 23:18:47 +0300

    Remove unused code

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ btn.addEventListener('click', function () {
             const setBg = () => {
                 const randomColor = Math.floor(Math.random() * 16777215).toString(16);
                 square.style.backgroundColor = "#" + randomColor;
-                color.innerHTML = "#" + randomColor;
             }
             setBg();
         });
