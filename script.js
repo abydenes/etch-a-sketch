c523631285c7837ec112659b06f5ca8a94ef5805
Abydenes <schoolreef2@protonmail.com>
2022-07-28 21:52:03 +0300

    Added button functionality
    The button now accepts a number less than 100 from the user and
    creates a new grid.

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-let gridSize = 0;
-
 const container = document.querySelector('#container');
 
-for (i = 0; i < 64; i++) {
-    let div = document.createElement('div');
-    div.classList.add('squares');
-    container.appendChild(div);
-}
 
 
-const squareHover = document.querySelectorAll('.squares');
-
-Array.from(squareHover).forEach(square => { 
-    square.addEventListener('mouseenter', function (e) {
-    square.classList.add('hoveredOver')});
-})
 
 const btn = document.querySelector('button')
 btn.addEventListener('click', function () {
-    gridSize = prompt('giv numba')
+    let gridSize = prompt('number?', 32);
+    if (gridSize > 100) {
+        alert('Number too big, NO NO NO!');
+        return
+    }
+
+    document.querySelector("div").innerHTML = ''
+
+    for (i = 0; i < gridSize ** 2; i++) {
+        let div = document.createElement('div');
+        div.classList.add('squares');
+        container.appendChild(div);
+
+        div.style.width = `${640 / gridSize}px`
+        div.style.height = `${640 / gridSize}px`
+    }
+    const squareHover = document.querySelectorAll('.squares');
+
+    Array.from(squareHover).forEach(square => {
+        square.addEventListener('mouseenter', function (e) {
+            square.classList.add('hoveredOver')
+        });
+    })
 })
+
